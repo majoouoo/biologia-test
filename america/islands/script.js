700a2e7eea56b4d987a902e6cb34e241f068029c
Majoouoo <26277754+Majoouoo@users.noreply.github.com>
2023-03-10 22:44:18 +0100

    added islands of america

---
+++ america/islands/script.js
@@ -0,0 +1,70 @@
+const mapNames = {
+    1: "Greenland",
+    2: "Baffin Island",
+    3: "Cuba",
+    4: "Jamaica",
+    5: "Hispaniola",
+    6: "Puerto Rico",
+    7: "lesser Antilles",
+    8: "Vancouver Island",
+    9: "Newfoundland",
+    10: "Victoria Island",
+    11: "Bahamas",
+    12: "Bermuda",
+    13: "Aleutian Islands"
+}
+
+const inputEl = document.getElementById("input")
+const checkBtn = document.getElementById("checkBtn")
+const resultEl = document.getElementById("result")
+const answerBtn = document.getElementById("answerBtn")
+
+const generateNum = () => {
+    const randomNum = Math.ceil(Math.random() * 13)
+    return randomNum
+}
+
+const checkAnswer = () => {
+    if (inputEl.value.toUpperCase() == mapNames[localStorage.getItem("randomNum")].toUpperCase()) {
+        resultEl.innerHTML = "Správne"
+        resultEl.style.color = "green"
+        // reset
+        inputEl.value = ""
+        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = ""
+        // generate new
+        const randomNum = generateNum()
+        localStorage.setItem("randomNum", randomNum)
+        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
+    } else if (inputEl.value.toUpperCase() != mapNames[localStorage.getItem("randomNum")].toUpperCase()) {
+        resultEl.innerHTML = "Zle"
+        resultEl.style.color = "red"
+    }
+}
+
+
+const randomNum = generateNum()
+localStorage.setItem("randomNum", randomNum)
+document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        checkAnswer()
+    }
+})
+
+checkBtn.addEventListener("click", () => {
+    checkAnswer()
+})
+
+answerBtn.addEventListener("click", () => {
+    resultEl.innerHTML = mapNames[localStorage.getItem("randomNum")]
+    resultEl.style.color = "black"
+    setTimeout(() => {
+        inputEl.value = ""
+        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = ""
+        const randomNum = generateNum()
+        localStorage.setItem("randomNum", randomNum)
+        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
+        resultEl.innerHTML = ""
+    }, 1500)
+})
