9d894c3f4205d17681f3a07f0105ce446908d4e1
Majoouoo <26277754+Majoouoo@users.noreply.github.com>
2023-03-12 14:58:26 +0100

    added hydrology of south america, common css grouped to default.css, strings are converted to uppercase without accents

--- america/islands/script.js
+++ america/islands/script.js
@@ -14,57 +14,86 @@ const mapNames = {
     13: "Aleutian Islands"
 }
 
+// declare elements
 const inputEl = document.getElementById("input")
-const checkBtn = document.getElementById("checkBtn")
-const resultEl = document.getElementById("result")
 const answerBtn = document.getElementById("answerBtn")
+const checkBtn = document.getElementById("checkBtn")
+const showAllBtn = document.getElementById("showAllBtn")
+const resultEl = document.getElementById("result")
+const mapList = document.getElementById("mapList")
+const mapBtns = document.getElementsByClassName("mapBtn")
 
+// generate a number and add a class
 const generateNum = () => {
-    const randomNum = Math.ceil(Math.random() * 13)
-    return randomNum
+    const chosenNum = Math.ceil(Math.random() * Object.keys(mapNames).length)
+    document.getElementById("b" + chosenNum).classList.add("activeBtn")
 }
 
+// convert string to uppercase without accents
+const convertString = (string) => {
+    return string.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+}
+
+// check answer
 const checkAnswer = () => {
-    if (inputEl.value.toUpperCase() == mapNames[localStorage.getItem("randomNum")].toUpperCase()) {
+    const chosenNum = document.getElementsByClassName("activeBtn")[0].id.substring(1)
+    const chosenEl = document.getElementsByClassName("activeBtn")[0]
+
+    if (convertString(inputEl.value) == convertString(mapNames[chosenNum])) {
         resultEl.innerHTML = "Správne"
         resultEl.style.color = "green"
         // reset
         inputEl.value = ""
-        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = ""
+        chosenEl.classList.remove("activeBtn")
         // generate new
-        const randomNum = generateNum()
-        localStorage.setItem("randomNum", randomNum)
-        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
-    } else if (inputEl.value.toUpperCase() != mapNames[localStorage.getItem("randomNum")].toUpperCase()) {
+        generateNum()
+    } else if (convertString(inputEl.value) != convertString(mapNames[chosenNum])) {
         resultEl.innerHTML = "Zle"
         resultEl.style.color = "red"
     }
 }
 
+// ok button + enter
+document.addEventListener('keypress', e => e.key === 'Enter' ? checkAnswer() : null)
+checkBtn.addEventListener("click", () => checkAnswer())
 
-const randomNum = generateNum()
-localStorage.setItem("randomNum", randomNum)
-document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
-
-document.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        checkAnswer()
-    }
-})
-
-checkBtn.addEventListener("click", () => {
-    checkAnswer()
-})
-
+// show correct answer
 answerBtn.addEventListener("click", () => {
-    resultEl.innerHTML = mapNames[localStorage.getItem("randomNum")]
+    const chosenNum = document.getElementsByClassName("activeBtn")[0].id.substring(1)
+    const chosenEl = document.getElementsByClassName("activeBtn")[0]
+
+    resultEl.innerHTML = mapNames[chosenNum]
     resultEl.style.color = "black"
+    answerBtn.disabled = true
     setTimeout(() => {
         inputEl.value = ""
-        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = ""
-        const randomNum = generateNum()
-        localStorage.setItem("randomNum", randomNum)
-        document.getElementById("b" + localStorage.getItem("randomNum")).style.background = "red"
+        chosenEl.classList.remove("activeBtn")
+        generateNum()
         resultEl.innerHTML = ""
+        answerBtn.disabled = false
     }, 1500)
 })
+
+// show list of map buttons
+const showMapList = () => {
+    const leftListCount = Math.ceil(Object.keys(mapNames).length / 2)
+
+    for (mapName in mapNames) {
+        const el = mapName <= leftListCount
+            ? leftList.appendChild(document.createElement("p"))
+            : rightList.appendChild(document.createElement("p"))
+
+        el.innerHTML = mapNames[mapName]
+        el.addEventListener("click", () => {
+            const mapBtn = "b" + Object.keys(mapNames).find(key => mapNames[key] === el.innerHTML)
+
+            document.getElementById(mapBtn).classList.add("visibleMapBtn")
+
+            setTimeout(() => document.getElementById(mapBtn).classList.remove("visibleMapBtn"), 1500)
+        })
+    }
+}
+
+// initialize
+generateNum()
+showMapList()
