e0a1f09c0c1f78805bf080159b7c4b45619c2f60
Majoouoo <26277754+Majoouoo@users.noreply.github.com>
2023-05-01 22:58:48 +0200

    added hydrology of africa

---
+++ africa/hydrology/script.js
@@ -0,0 +1,118 @@
+const mapNames = {
+    1: "Nile",
+    2: "White Nile",
+    3: "Blue Nile",
+    4: "Senegal",
+    5: "Niger",
+    6: "Volta",
+    7: "Congo",
+    8: "Zambezi",
+    9: "Limpopo",
+    10: "Orange",
+    11: "Lake Chad",
+    12: "Lake Albert",
+    13: "Lake Turkana",
+    14: "Lake Victoria",
+    15: "Tanganyika",
+    16: "Malawi",
+    17: "Lake Nasser",
+    18: "Victoria Falls",
+    19: "Okavango Delta"
+}
+
+// declare elements
+const inputEl = document.getElementById("input")
+const answerBtn = document.getElementById("answerBtn")
+const checkBtn = document.getElementById("checkBtn")
+const showAllBtn = document.getElementById("showAllBtn")
+const resultEl = document.getElementById("result")
+const mapList = document.getElementById("mapList")
+const mapBtns = document.getElementsByClassName("mapBtn")
+const hideBtn = document.getElementById("hideBtn")
+
+// generate a number and add a class
+const generateNum = () => {
+    const chosenNum = Math.ceil(Math.random() * Object.keys(mapNames).length)
+    document.getElementById("b" + chosenNum).classList.add("activeBtn")
+}
+
+// convert string to uppercase without accents
+const convertString = (string) => {
+    return string.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+}
+
+// check answer
+const checkAnswer = () => {
+    const chosenNum = document.getElementsByClassName("activeBtn")[0].id.substring(1)
+    const chosenEl = document.getElementsByClassName("activeBtn")[0]
+
+    if (convertString(inputEl.value) == convertString(mapNames[chosenNum])) {
+        resultEl.innerHTML = "Správne"
+        resultEl.style.color = "green"
+        // reset
+        inputEl.value = ""
+        chosenEl.classList.remove("activeBtn")
+        // generate new
+        generateNum()
+    } else {
+        resultEl.innerHTML = "Zle"
+        resultEl.style.color = "red"
+    }
+}
+
+// ok button + enter
+document.addEventListener('keypress', e => e.key === 'Enter' ? checkAnswer() : null)
+checkBtn.addEventListener("click", () => checkAnswer())
+
+// show correct answer
+answerBtn.addEventListener("click", () => {
+    const chosenNum = document.getElementsByClassName("activeBtn")[0].id.substring(1)
+    const chosenEl = document.getElementsByClassName("activeBtn")[0]
+
+    resultEl.innerHTML = mapNames[chosenNum]
+    resultEl.style.color = "black"
+    answerBtn.disabled = true
+    setTimeout(() => {
+        inputEl.value = ""
+        chosenEl.classList.remove("activeBtn")
+        generateNum()
+        resultEl.innerHTML = ""
+        answerBtn.disabled = false
+    }, 1500)
+})
+
+// show list of map buttons
+const showMapList = () => {
+    const leftListCount = Math.ceil(Object.keys(mapNames).length / 2)
+
+    for (mapName in mapNames) {
+        const el = mapName <= leftListCount
+            ? leftList.appendChild(document.createElement("p"))
+            : rightList.appendChild(document.createElement("p"))
+
+        el.innerHTML = mapNames[mapName]
+        el.addEventListener("click", () => {
+            const mapBtn = "b" + Object.keys(mapNames).find(key => mapNames[key] === el.innerHTML)
+
+            document.getElementById(mapBtn).classList.add("visibleMapBtn")
+
+            setTimeout(() => document.getElementById(mapBtn).classList.remove("visibleMapBtn"), 1500)
+        })
+    }
+}
+
+// hide list
+mapList.style.display = "grid"
+hideBtn.addEventListener("click", () => {
+    if (mapList.style.display == "grid") {
+        mapList.style.display = "none"
+        hideBtn.innerHTML = "Show list"
+    } else {
+        mapList.style.display = "grid"
+        hideBtn.innerHTML = "Hide list"
+    }
+})
+
+// initialize
+generateNum()
+showMapList()
